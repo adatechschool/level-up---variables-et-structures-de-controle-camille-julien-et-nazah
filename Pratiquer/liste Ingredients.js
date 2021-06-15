f5b5ad3313dd81794171114ac5ccddd0307d4288
jhilaa <julien.hilaire@imelmi.net>
2021-06-15 12:14:15 +0200

    commit

---
+++ Pratiquer/liste Ingredients.js
@@ -0,0 +1,29 @@
+tabIngredients = ['Crème','Pâtes', 'Oeuf', 'Sel', 'Poivre', 'Lardon', 'Oignon'];
+
+let resumeRecette = document.getElementById("resumeRecette");
+let listeIngredients = document.getElementById("listeIngredients");
+let listeIngredientsOrdonnée = document.getElementById("listeIngredientsOrdonnée");
+
+let premierIngredient = true;
+
+for (const ingredient of tabIngredients){ 
+
+	resumeRecette.textContent += (premierIngredient ? "" : ", ") + ingredient;
+	premierIngredient = false;
+	//
+
+	let newIngredientLigne = document.createElement("li");
+	let newIngredient = document.createTextNode(ingredient);
+	newIngredientLigne.appendChild(newIngredient);
+	listeIngredients.appendChild(newIngredientLigne);
+}
+
+
+// liste ordonnée
+for (const ingredient of tabIngredients.sort()){ 
+
+	let newIngredientLigne = document.createElement("li");
+	let newIngredient = document.createTextNode(ingredient);
+	newIngredientLigne.appendChild(newIngredient);
+	listeIngredientsOrdonnée.appendChild(newIngredientLigne);
+}
